30c3dd02545ca09f15c42ab8731ef767c6ade00d
Tomáš Schmidl <schmidltomas@gmail.com>
2020-06-03 09:05:36 +0200

    Draw snake correctly in initial position

--- snake.py
+++ snake.py
@@ -3,12 +3,15 @@ from enum import Enum
 
 
 class Point:
-	'''Base class for all points on the board.'''
+	'''Base class for all points on the board with x and y coordinates.'''
+	x = 0
+	y = 0
+
 	def __init__(self, x, y):
 		self.x = x
 		self.y = y
-		
-	def draw(self):
+
+	def draw(self, x, y):
 		print('.', end='')
 
 
@@ -17,22 +20,28 @@ class Food(Point):
 	def __init__(self, x, y):
 		Point.__init__(self, x, y)
 		
-	def draw(self):
+	def draw(self, x, y):
 		print('X', end='')
-		
+
 		
 class Snake(Point):
-	'''The snake itself.'''
-	head
-	tail = []
+	'''The snake itself - represented by a head Point and list of tail Points.'''
+	tail = [Point]
+	head = Point
 	
 	def __init__(self, x, y):
 		Point.__init__(self, x, y)
-		head = Point(x, y)
-		tail = [Point(x, y-1), Point(x, y-2), Point(x, y-3)]
+		self.head = Point(x, y)
+		self.tail = [Point(x, y-1), Point(x, y-2), Point(x, y-3)]
 
 	def draw(self):
 		print('O', end='')
+
+	def draw(self, x, y):
+		if x == self.head.x and y == self.head.y:
+			print('O', end='')
+		else:
+			print('o', end='')
 
 
 class Direction(Enum):
@@ -52,6 +61,7 @@ class Direction(Enum):
 		elif key == 'd':
 			return Direction.RIGHT
 
+
 class Board():
 	'''Playing board.'''
 	direction = Direction.RIGHT
@@ -64,46 +74,75 @@ class Board():
 		self.board[food.x][food.y] = food
 	
 	def draw(self):
+		# find snake head on board
+		# TODO to find_head() function
+		debug = ""
 		for i in range(self.width):
 			for j in range(self.height):
 				if isinstance(self.board[i][j], Snake):
 					snake = self.board[i][j]
-					for tail_point in snake.tail:
-						self.board[tail_point.x][tail_point.y] = Snake(0, 0)
-				self.board[i][j].draw()
+					if i == snake.head.x and j == snake.head.y:
+						debug += "snake head at {0},{1}\n".format(i, j)
+						# TODO needed?
+						# self.board[snake.head.x][snake.head.y] = snake
+						# draw snake tail on board
+						for tail_point in snake.tail:
+							self.board[tail_point.x][tail_point.y] = snake
+							debug += "snake tail at {0},{1}\n".format(tail_point.x, tail_point.y)
+		print(debug)
+
+		# draw board
+		for i in range(self.width):
+			for j in range(self.height):
+				self.board[i][j].draw(i, j)
+				# print(self.board[i][j])
 			print()
 
+	# TODO use only x+y or i+j
 	def move(self, direction):
 		for i in range(self.width):
 			for j in range(self.height):
 				if isinstance(self.board[i][j], Snake):
-					if direction == Direction.UP:
-						self.board[i][j] = Point(0,0)
-						self.board[i - 1][j] = Snake(0, 0)
-						return
-					elif direction == Direction.DOWN:
-						self.board[i][j] = Point(0,0)
-						self.board[i + 1][j] = Snake(0, 0)
-						return
-					elif direction == Direction.LEFT:
-						self.board[i][j] = Point(0,0)
-						self.board[i][j - 1] = Snake(0, 0)
-						return
-					elif direction == Direction.RIGHT:
-						self.board[i][j] = Point(0,0)
-						self.board[i][j + 1] = Snake(0, 0)
-						return
+					snake = self.board[i][j]
+					if i == snake.head.x and j == snake.head.y:
+						# move last element of tail to where head is
+						last_tail_point = snake.tail[len(snake.tail)-1]
+						last_tail_point.x = snake.head.x
+						last_tail_point.y = snake.head.y
+						print("last tail point: {0},{1}".format(last_tail_point.x, last_tail_point.y))
+						self.board[last_tail_point.x][last_tail_point.y] = Point
+
+						if direction == Direction.UP:
+							# move head in the direction
+							snake.head.x -= 1
+							# update snake head position on the board
+							self.board[i - 1][j] = snake
+							return
+						elif direction == Direction.DOWN:
+							snake.head.x += 1
+							self.board[i + 1][j] = snake
+							return
+						elif direction == Direction.LEFT:
+							snake.head.y -= 1
+							self.board[i][j - 1] = snake
+							return
+						elif direction == Direction.RIGHT:
+							snake.head.y += 1
+							self.board[i][j + 1] = snake
+							return
 			
 
 if __name__ == '__main__':
 	try:
 		board = Board(11, 19, Snake(5, 9), Food(7, 14))
+		board.draw()
 		while True:
-			board.draw()
 			mode=input('Input: ')
 			board.move(Direction.key(mode))
+			board.draw()
 		
-	except ValueError:
-		print("Err")
+	except KeyboardInterrupt:
+		print()
+		pass
 
 
