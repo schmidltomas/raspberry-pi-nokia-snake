4dbf5f85bd8fa1ab4b13e5d3981b638e6c3d4a27
Tomáš Schmidl <schmidltomas@gmail.com>
2020-06-09 13:55:13 +0200

    Implemented non-blocking keyboard input

--- snake.py
+++ snake.py
@@ -2,6 +2,12 @@
 from enum import Enum
 
 import random
+import fcntl
+import sys
+import os
+import time
+import tty
+import termios
 
 
 class Point:
@@ -21,7 +27,7 @@ class Food(Point):
 	"""Food for the snake."""
 	def __init__(self, x, y):
 		Point.__init__(self, x, y)
-		
+
 	def draw(self, x, y):
 		print('X', end='')
 
@@ -33,7 +39,7 @@ class Direction(Enum):
 	LEFT = 3
 	RIGHT = 4
 
-	def key(self):
+	def from_key(self):
 		if self == 'w':
 			return Direction.UP
 		elif self == 's':
@@ -43,38 +49,40 @@ class Direction(Enum):
 		elif self == 'd':
 			return Direction.RIGHT
 
-		
+
 class Snake(Point):
 	"""The snake itself - represented by a list of body Points."""
 	body = [Point]
 	direction = Direction.RIGHT
-	
+
 	def __init__(self, x, y):
 		Point.__init__(self, x, y)
 		self.body = [Point(x, y), Point(x, y - 1), Point(x, y - 2), Point(x, y - 3)]
-		
+
 	def head(self):
 		return self.body[0]
 
 	def tail(self):
 		return self.body[len(self.body) - 1]
-		
+
 	def eat(self):
 		self.body.append(Point(self.tail().x, self.tail().y))
 
 	def move_body(self):
-		k = len(self.body) - 1 
+		k = len(self.body) - 1
 		while len(self.body) > k > 0:
 			self.body[k].x = self.body[k - 1].x
 			self.body[k].y = self.body[k - 1].y
 			k -= 1
 
-	def check_direction(self, direction):
-		if direction == Direction.UP and self.direction == Direction.DOWN \
-				or direction == Direction.DOWN and self.direction == Direction.UP \
-				or direction == Direction.LEFT and self.direction == Direction.RIGHT \
-				or direction == Direction.RIGHT and self.direction == Direction.LEFT:
-			raise DirectionException
+	def valid_direction(self, new_direction):
+		if new_direction == Direction.UP and self.direction == Direction.DOWN \
+				or new_direction == Direction.DOWN and self.direction == Direction.UP \
+				or new_direction == Direction.LEFT and self.direction == Direction.RIGHT \
+				or new_direction == Direction.RIGHT and self.direction == Direction.LEFT:
+			return False
+		else:
+			return True
 
 	def draw(self, x, y):
 		if x == self.body[0].x and y == self.body[0].y:
@@ -88,14 +96,14 @@ class Board:
 	snake = Snake
 	food = Food
 	score = 0
-	
+
 	def __init__(self, width, height, snake, food):
 		self.width = width
 		self.height = height
 		self.board = [[Point(0, 0) for x in range(self.height)] for y in range(self.width)]
 		self.snake = snake
 		self.food = food
-	
+
 	def draw(self):
 		# draw snake on board
 		for body_point in self.snake.body:
@@ -135,30 +143,30 @@ class Board:
 		# move the snake's body (without head)
 		self.snake.move_body()
 
-	def move(self, direction):
+	def move(self, new_direction):
 		# check if snake's new direction is valid
-		self.snake.check_direction(direction)
-		self.snake.direction = direction
+		if self.snake.valid_direction(new_direction):
+			self.snake.direction = new_direction
 
 		head = self.snake.head()
-		if direction == Direction.UP:
+		if self.snake.direction == Direction.UP:
 			self.spawn_new_food(self.board[head.x - 1][head.y])
 			# update snake head position on the board
 			self.board[head.x - 1][head.y] = Snake(0, 0)
 			# move head in the direction
 			head.x -= 1
 			return
-		elif direction == Direction.DOWN:
+		elif self.snake.direction == Direction.DOWN:
 			self.spawn_new_food(self.board[head.x + 1][head.y])
 			self.board[head.x + 1][head.y] = Snake(0, 0)
 			head.x += 1
 			return
-		elif direction == Direction.LEFT:
+		elif self.snake.direction == Direction.LEFT:
 			self.spawn_new_food(self.board[head.x][head.y - 1])
 			self.board[head.x][head.y - 1] = Snake(0, 0)
 			head.y -= 1
 			return
-		elif direction == Direction.RIGHT:
+		elif self.snake.direction == Direction.RIGHT:
 			self.spawn_new_food(self.board[head.x][head.y + 1])
 			self.board[head.x][head.y + 1] = Snake(0, 0)
 			head.y += 1
@@ -169,25 +177,65 @@ class DirectionException(Exception):
 	pass
 
 
+class Raw(object):
+	"""Raw wrapper for stdin. (http://ballingt.com/nonblocking-stdin-in-python-3/)"""
+	def __init__(self, stream):
+		self.stream = stream
+		self.fd = self.stream.fileno()
+	def __enter__(self):
+		self.original_stty = termios.tcgetattr(self.stream)
+		tty.setcbreak(self.stream)
+	def __exit__(self, type, value, traceback):
+		termios.tcsetattr(self.stream, termios.TCSANOW, self.original_stty)
+
+
+class NonBlocking(object):
+	"""Non-blocking wrapper for stdin. (http://ballingt.com/nonblocking-stdin-in-python-3/)"""
+	def __init__(self, stream):
+		self.stream = stream
+		self.fd = self.stream.fileno()
+	def __enter__(self):
+		self.orig_fl = fcntl.fcntl(self.fd, fcntl.F_GETFL)
+		fcntl.fcntl(self.fd, fcntl.F_SETFL, self.orig_fl | os.O_NONBLOCK)
+	def __exit__(self, *args):
+		fcntl.fcntl(self.fd, fcntl.F_SETFL, self.orig_fl)
+
+
 if __name__ == '__main__':
+	# turn time in seconds - the time it takes to refresh the board
+	turn_time = 0.5
+	# initial direction is right
+	direction = Direction.RIGHT
+
+	# init board size, snake and food position
+	board = Board(11, 19, Snake(5, 9), Food(6, 12))
+	board.draw()
+	board.print_score()
+
 	try:
-		board = Board(11, 19, Snake(5, 9), Food(6, 12))
-		board.draw()
-		board.print_score()
-		while True:
-			mode = input('Input: ')
-			if mode in ['w', 's', 'a', 'd']:
-				board.move(Direction.key(mode))
-				board.draw()
-				board.print_score()
-			else:
-				print('Invalid input!')
+		last_update = time.time()
 
-	except DirectionException:
-		print('Invalid snake direction!')
-		pass
+		with Raw(sys.stdin):
+			with NonBlocking(sys.stdin):
+				while True:
+					# read input key from stdin
+					key = sys.stdin.read(1)
+					if key in ['w', 's', 'a', 'd']:
+						direction = Direction.from_key(key)
+					elif key == '\x1b':
+						# x1b is ESC - ends the game
+						break
+
+					time.sleep(0.1)
+
+					# once in a specified turn_time, move the board and redraw it
+					if time.time() - last_update > turn_time:
+						board.move(direction)
+						board.draw()
+						board.print_score()
+						last_update = time.time()
+
+	except IOError:
+		print('I/O not ready.')
 	except KeyboardInterrupt:
-		print()
 		pass
-
-
